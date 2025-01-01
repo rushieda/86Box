f1888aa0d84f4827453703e6fc0caf45c80f0787
rushieda <185547947+rushieda@users.noreply.github.com>
2025-01-01 23:27:29 +0300

    Add the Dell NetPlex 4xx/P

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -544,6 +544,7 @@ extern int machine_at_mvi486_init(const machine_t *);
 extern int machine_at_sis401_init(const machine_t *);
 extern int machine_at_isa486_init(const machine_t *);
 extern int machine_at_av4_init(const machine_t *);
+extern int machine_at_dell4xxp_init(const machine_t *);
 extern int machine_at_valuepoint433_init(const machine_t *);
 
 extern int machine_at_vli486sv2g_init(const machine_t *);
